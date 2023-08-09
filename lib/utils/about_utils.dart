52bcdcd4d2f5076ffb9fc2c9f98cc994b04a3448
BradMyrick <brad.myrick@protonmail.com>
2023-08-09 11:52:46 -0500

    well flutter doesn't like on the fly calculations so I'll just drop age.

--- lib/utils/about_utils.dart
+++ lib/utils/about_utils.dart
@@ -1,10 +1,6 @@
 class AboutUtils {
   static const String name = "Brad Myrick";
   // calculate age from date of birth
-  static const Int birthYear = 1987;
-  static const Int birthMonth = 03;
-  static const Int birthDay = 04;
-  static const String age = calculateAge(birthYear, birthMonth, birthDay);
   static const String email = "brad@kodr.pro";
   static const String location = "Decentralized";
   static const String aboutMeHeadline =
@@ -14,14 +10,3 @@ class AboutUtils {
       "A little background on me. I'm a US Army veteran with a Bachelor's in Computer Science. I have extensive experience building and deploying Dapps on EVM-compatible blockchains and work full-time as a Systems Engineer for Trugard.ai, a web3 cybersecurity company. I also contribute to Web3 Opensource and am a proud member of the Fudderverse and Lazy Lion NFT community.";
 
 }
-
-calculateAge(birthYear, birthMonth, birthDay) {
-  var today = new Date();
-  var age = today.getFullYear() - birthYear;
-  var m = today.getMonth() - birthMonth;
-  if (m < 0 || (m === 0 && today.getDate() < birthDay)) {
-    age--;
-  }
-  return age;
-}
-
