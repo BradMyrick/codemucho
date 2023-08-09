ae8fc3ef29df50ac57c1a36b64ace91428032092
BradMyrick <brad.myrick@protonmail.com>
2023-08-09 11:47:23 -0500

    2023 updates and a calculateage function so i don't have to deal with it next year.

--- lib/utils/about_utils.dart
+++ lib/utils/about_utils.dart
@@ -1,12 +1,27 @@
 class AboutUtils {
   static const String name = "Brad Myrick";
-  static const String age = "35";
-  static const String email = "kodr@codemucho.com";
-  static const String location = "Santa Marta, Colombia";
+  // calculate age from date of birth
+  static const Int birthYear = 1987;
+  static const Int birthMonth = 03;
+  static const Int birthDay = 04;
+  static const String age = calculateAge(birthYear, birthMonth, birthDay);
+  static const String email = "brad@kodr.pro";
+  static const String location = "Decentralized";
   static const String aboutMeHeadline =
-      "Hey I'm Kodr, a Web3 focused Full-Stack Engineer.";
+      "GM! I'm Kodr, a Web3 focused Full-Stack Engineer.";
 
   static const String aboutMeDetail =
-      "I am a US Army combat veteran with a Bachelor's in Computer Science from Full Sail University in Orlando, Florida.\nI have extensive experience building and deploying Dapps on EVM-compatible networks and work full-time as a Systems Engineer for Trugard Labs, a web3 cybersecurity company. I am also a contributor to the Ethereum Foundation's Security Research Community and a proud member of the Fudderverse NFT community.";
+      "A little background on me. I'm a US Army veteran with a Bachelor's in Computer Science. I have extensive experience building and deploying Dapps on EVM-compatible blockchains and work full-time as a Systems Engineer for Trugard.ai, a web3 cybersecurity company. I also contribute to Web3 Opensource and am a proud member of the Fudderverse and Lazy Lion NFT community.";
 
 }
+
+calculateAge(birthYear, birthMonth, birthDay) {
+  var today = new Date();
+  var age = today.getFullYear() - birthYear;
+  var m = today.getMonth() - birthMonth;
+  if (m < 0 || (m === 0 && today.getDate() < birthDay)) {
+    age--;
+  }
+  return age;
+}
+
