537d91f017d9c0c6a5414033f414788058cbeb50
BradMyrick <brad.myrick@protonmail.com>
2023-09-24 13:23:32 -0500

    NEXT App time bae bae

--- lib/utils/contact_utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ContactUtils {
-  static const IconData contactIcon = Icons.mail;
-
-  static const String title = "Email";
-
-  static const String details = "kodr@kodr.pro";
-}
